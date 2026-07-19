da3b1433eccd5c4c80dd38b38337c5b808a6df21
Nelvin Mwendwa <mwendwanelvin6@gmail.com>
2026-07-19 08:20:20 +0300

    Release v1.0.0 - Production ready

--- app.py
+++ app.py
@@ -359,5 +359,18 @@ def docs():
 
     return render_template("docs.html")
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
